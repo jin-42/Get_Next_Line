57ba0f802d9ff21e615ff2b3ffebff301ccf32c4
jin <faruksulvac@gmail.com>
2023-12-03 12:06:30 +0100

    tryharder..

---
+++ Version3/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsulvac <fsulvac@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 19:09:12 by fsulvac           #+#    #+#             */
+/*   Updated: 2023/12/02 19:21:18 by fsulvac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif 
+
+char *get_next_line(int fd);
+
+#endif 
+
